7bc48f2a29a9b86f97c137ad31a195743dce76bf
Jesus <al221710026@gmail.com>
2021-03-10 17:38:45 -0600

    Se agrego archivo nuevo (vista blade politica)

---
+++ resources/views/electronica/politica.blade.php
@@ -0,0 +1,166 @@
+@extends('electronica.index')
+
+@section('contenido')
+<!------------------------------------------------------------------------------------------------------------------>
+
+
+
+<p>
+    AVISO DE PRIVACIDAD
+El presente Aviso de Privacidad se pone a disposición del TITULAR, en cumplimiento a lo dispuesto
+por la Ley Federal de Protección de Datos Personales en posesión de los Particulares, así como demás
+disposiciones legales aplicables.
+Jesus Chicho Hernández, mejor conocido como Software UTVT, con domicilio en calle
+Avenida 16 de septiembre, colonia san Martín, ciudad Estado de México, municipio o
+delegación Huixquilucan, cp. 52760, en la entidad de México, país México, es el responsable
+del uso y protección de sus datos personales, con lo dispuesto en la Ley Federal de Protección
+de Datos Personales en Posesión de los Particulares hace de su conocimiento el presente
+Aviso de Privacidad así como demás disposiciones legales aplicables y al respecto le
+informamos lo siguiente:
+¿Como protegeremos sus datos?
+Se ejercerán las leyes y normas establecidas por la ley para su protección que son:
+• Ley general de protección de datos personales en posesión de los Particulares.
+• ISO 27001
+¿Para qué fines utilizaremos sus datos personales?
+Los datos personales que recabamos de usted, los utilizaremos para las siguientes
+finalidades que son necesarias para el servicio que solicita:
+• Generación de Facturas
+• Comunicarle sobre los cambios en los servicios, productos e información que ha
+solicitado.
+• Creación del perfil personal
+• Generación de control de clientes
+De manera adicional, utilizaremos su información personal para las siguientes finalidades
+secundarias que no son necesarias para el servicio solicitado, pero que nos permiten y
+facilitan brindarle una mejor atención:
+• Generación de control de clientes
+• Proveer nuestros servicios
+En caso de que no desee que sus datos personales se utilicen para estos fines secundarios,
+indíquelo a continuación:
+No consiento que mis datos personales se utilicen para los siguientes fines:
+[ ] Encuestas
+[ ] Proveer nuestros servicios
+[ ] Inscripción al servicio de notificación de ofertas especiales a través de correo electrónico
+P á g i n a 5 | 10
+La negativa para el uso de sus datos personales para estas finalidades no podrá ser un motivo
+para que le neguemos los servicios y productos que solicita o contrata con nosotros.
+Software UTVT responsable del tratamiento de tus datos personales, está obligada a cumplir
+con los principios de licitud, consentimiento, información, calidad, finalidad, lealtad,
+proporcionalidad y responsabilidad tutelados en la Ley; por tal motivo con fundamento en el
+artículo 14 de la Ley.
+Cabe informar que esté presente documento hace referencia con lo dispuesto en el artículo
+16
+¿Qué datos personales utilizaremos para estos fines?
+Para llevar a cabo las finalidades descritas en el presente aviso de privacidad, utilizaremos
+los siguientes datos personales:
+• Datos de identificación
+• Datos de contacto
+• Datos biométricos
+• Datos patrimoniales y/o financieros
+¿Cómo puede acceder, rectificar o cancelar sus datos personales, u oponerse a su
+uso?
+Usted tiene derecho a conocer qué datos personales tenemos de usted, para qué los utilizamos
+y las condiciones del uso que les damos (Acceso). Asimismo, es su derecho solicitar la
+corrección de su información personal en caso de que esté desactualizada, sea inexacta o
+incompleta (Rectificación); que la eliminemos de nuestros registros o bases de datos cuando
+considere que la misma no está siendo utilizada adecuadamente (Cancelación); así como
+oponerse al uso de sus datos personales para fines específicos (Oposición). Estos derechos
+se conocen como derechos ARCO.
+Para conocer el procedimiento y requisitos para el ejercicio de los derechos ARCO, ponemos
+a su disposición el siguiente medio:
+Presencial con el Personal en el área designada a disposición
+Vía Electrónica: Cuenta de correo SoftwareUTVT@gmail.com
+
+P á g i n a 6 | 10
+¿Cómo puede limitar el uso o divulgación de su información personal?
+Con objeto de que usted pueda limitar el uso y divulgación de su información personal, le
+ofrecemos los siguientes medios:
+Solicitud en el área designada
+De manera adicional, le informamos que contamos con los siguientes listados de exclusión,
+en los cuales podrá registrarse para que sus datos personales no sean tratados para ciertos
+fines:
+Los datos de contacto de la persona o departamento de datos personales, que está a cargo
+de dar trámite a las solicitudes de derechos ARCO, son los siguientes:
+a) Nombre de la persona o departamento de datos personales: Jesus Chicho Hernández
+b) Domicilio: calle Avenida 16 de septiembre, colonia San Martin, ciudad Estado de México,
+municipio o delegación Huixquilucan, cp. 52760, en la entidad de México, país México
+c) Correo electrónico: SoftwareUTVT@gmail.com
+d) Número telefónico: 5529983634
+Usted puede revocar su consentimiento para el uso de sus datos personales
+Con lo dispuesto en el artículo 8.
+Usted tiene derecho y puede revocar el consentimiento que, en su caso, nos haya otorgado
+para el tratamiento de sus datos personales. Sin embargo, es importante que tenga en cuenta
+que no en todos los casos podremos atender su solicitud o concluir el uso de forma inmediata,
+ya que es posible que por alguna obligación legal requiramos seguir tratando sus datos
+personales. Asimismo, usted deberá considerar que, para ciertos fines, la revocación de su
+consentimiento implicará que no le podamos seguir prestando el servicio que nos solicitó, o la
+conclusión de su relación con nosotros.
+Para revocar su consentimiento deberá presentar su solicitud a través del siguiente medio:
+Documento de solicitud del tramite
+Para conocer el procedimiento y requisitos para la revocación del consentimiento, ponemos a
+su disposición el siguiente medio:
+Presencial con el [personal y área designada como titular
+P á g i n a 7 | 10
+Nombre del listado Finalidad para
+las que aplica Medio para obtener mayor información
+Avisos de datos
+personales en uso de
+publicidad
+Publicidad número telefónico 552983634, correo electrónico
+SoftwareUTVT@gmail.com, oficinas
+Asimismo, usted se podrá inscribir a los siguientes registros, en caso de que no desee
+obtener publicidad de nuestra parte:
+Registro Público para Evitar Publicidad, para mayor información consulte el portal de internet
+de la PROFECO
+El uso de tecnologías de rastreo en nuestro portal de internet
+Le informamos que en nuestra página de internet utilizamos cookies, web beacons u otras
+tecnologías, a través de las cuales es posible monitorear su comportamiento como usuario
+de internet, así como brindarle un mejor servicio y experiencia al navegar en nuestra página.
+Los datos personales que recabamos a través de estas tecnologías, los utilizaremos para los
+siguientes fines:
+• Reconocerlo al momento de entrar a nuestros sitios y ofrecerle de una experiencia
+personalizada
+• Conocer la configuración personal del sitio especificada por usted, por ejemplo, las
+cookies nos permiten detectar el ancho de banda que usted ha seleccionado al
+momento de ingresar al home page de nuestros sitios, de forma que sabemos qué tipo
+de información es aconsejable descargar
+• Calcular el tamaño de nuestra audiencia y medir algunos parámetros de tráfico, pues
+cada navegador que obtiene acceso a nuestros sitios adquiere una cookie que se usa
+para determinar la frecuencia de uso y las secciones de los sitios visitadas, reflejando
+así sus hábitos y preferencias, información que nos es útil para mejorar el contenido,
+los titulares y las promociones para los usuarios
+Los datos personales que obtenemos de estas tecnologías de rastreo son los siguientes:
+• Identificadores, nombre de usuario y contraseñas de una sesión
+• Idioma preferido por el usuario
+• Región en la que se encuentra el usuario
+• Fecha y hora del inicio y final de una sesión de un usuario
+• Búsquedas realizadas por un usuario
+• Publicidad revisada por un usuario
+• Listas y hábitos de consumo en páginas de compras
+P á g i n a 8 | 10
+Nombre y firma autógrafa del titular (en caso de requerirse conforme a la Ley).
+Nombre:
+Firma:
+Para conocer la forma en que se pueden deshabilitar estas tecnologías, consulte el siguiente
+medio:
+Cuando el usuario haga uso de la misma tecnología previamente mencionada
+¿Cómo puede conocer los cambios en este aviso de privacidad?
+Nos reservamos el derecho de efectuar en cualquier momento modificaciones o
+actualizaciones al presente aviso de privacidad, para la atención de novedades legislativas o
+jurisprudenciales, políticas internas de nuestras propias necesidades por los productos o
+servicios que ofrecemos; de nuestras prácticas de privacidad; de cambios en nuestro modelo
+de negocio, o por otras causas.
+Nos comprometemos a mantenerlo informado sobre los cambios que pueda sufrir el presente
+aviso de privacidad, a través de:
+Medios electrónicos:
+En nuestra página de Internet www.SoftwareUTVT.mx, sección aviso de privacidad o se las
+haremos llegar por los medios de comunicación que usted nos haya proporcionado.
+Área de residencia asignada.
+El procedimiento a través del cual se llevarán a cabo las notificaciones sobre cambios o
+actualizaciones al presente aviso de privacidad es el siguiente:
+Se informará previamente del aviso de privacidad de cuándo cambiará.
+La fecha de la última actualización al presente aviso de privacidad: [día/mes/ año]
+</p>
+
+
+<!------------------------------------------------------------------------------------------------------------------>
+@stop
